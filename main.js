eab3d932d94370825bd436c72e8fd4657e5e9afd
jessimeteo <jessica.laengle@uibk.ac.at>
2025-04-16 18:09:41 +0200

    Pop-up Bushaltestellen angepasst

--- main.js
+++ main.js
@@ -109,7 +109,7 @@ async function loadLines(url) {
                 weight: 3
             }
         },onEachFeature: function (feature, layer) {
-            console.log(feature.properties);
+            //console.log(feature.properties);
             layer.bindPopup(`
                 <h4> <i class="fa-solid fa-bus"></i> ${feature.properties.LINE_NAME} </h4>
                 <i class="fa-regular fa-circle-stop"></i> ${feature.properties.FROM_NAME} <br>
@@ -141,7 +141,7 @@ async function loadStops(url) {
             //console.log(feature.properties);
             layer.bindPopup(`
                 <h4> <i class="fa-solid fa-bus"></i> ${feature.properties.LINE_NAME} </h4>
-                `);
+                ${feature.properties.STAT_ID} ${feature.properties.STAT_NAME}`);
         }
     }).addTo(overlays.stops);
 }
@@ -205,7 +205,7 @@ async function loadHotels(url) {
 
 // GeoJSON laden und visualisieren
 //loadSights('https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json');
-loadLines('https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=json');
-//loadStops('https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKHTSVSLOGD&srsName=EPSG:4326&outputFormat=json');
+//loadLines('https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=json');
+loadStops('https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKHTSVSLOGD&srsName=EPSG:4326&outputFormat=json');
 //loadZones('https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:FUSSGEHERZONEOGD&srsName=EPSG:4326&outputFormat=json');
 //loadHotels('https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:UNTERKUNFTOGD&srsName=EPSG:4326&outputFormat=json');
